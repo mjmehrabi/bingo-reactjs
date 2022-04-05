4146c3f3675dfc2aad423e9a1d0800c678f1df08
Safer <safermehr@gmail.com>
2022-04-06 00:06:06 +0430

    fix timer

--- src/components/bingo/Board.tsx
+++ src/components/bingo/Board.tsx
@@ -34,7 +34,7 @@ function Board(props: any) {
             setAnimated(true)
             const timer = setTimeout(()=>{
                 setAnimated(false)
-            }, 2000)
+            }, 1000)
             return() => {
                 clearTimeout(timer)
             }
